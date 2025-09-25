ec3a77ccfbda59d9493fe748ca8ab97ba0af955b
ph0t0shop <lankreijersten@gmail.com>
2025-09-25 11:01:20 +0200

    Add push source to nuget push command

--- .github/scripts/nuget-publish.sh
+++ .github/scripts/nuget-publish.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 for match in ./JellyfinLoaderStub/build/*/bin/Release/*.nupkg; do
-  dotnet nuget push "$match" --api-key "$NUGET_PUBLISH_KEY"
+  dotnet nuget push "$match" --api-key "$NUGET_PUBLISH_KEY" --source https://api.nuget.org/v3/index.json
 done
